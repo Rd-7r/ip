140b201fdb8c97a464fedc4268cadfe17d6c356e
Rd-7r <mjiahao7r@gmail.com>
2026-08-19 14:50:24 +0800

    Add increment (greet & goodbye)

---
+++ src/main/java/Gizmo.java
@@ -0,0 +1,11 @@
+public class Gizmo {
+    public static void main(String[] args) {
+        String banner = "________________________________________________________________________________________\n"
+                + "Hello! I'm Gizmo. \n"
+                + "How may I assist you today?\n"
+                + "________________________________________________________________________________________\n"
+                + "Disconnecting... I'll be here whenever you need me again.\n"
+                + "________________________________________________________________________________________\n";
+        System.out.println(banner);
+    }
+}
