03626fdbb7c23be977af11e0f9c02eeac2511ed8
Rd-7r <mjiahao7r@gmail.com>
2026-08-28 12:53:00 +0800

    added Echo functionality

--- src/main/java/Gizmo.java
+++ src/main/java/Gizmo.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Gizmo {
-    public static void main(String[] args) {
-        String banner = "________________________________________________________________________________________\n"
-                + "Hello! I'm Gizmo. \n"
-                + "How may I assist you today?\n"
-                + "________________________________________________________________________________________\n"
-                + "Disconnecting... I'll be here whenever you need me again.\n"
-                + "________________________________________________________________________________________\n";
+    public static void main() {
+        String banner = "    ________________________________________________________________________________________\n"
+            + "    Hello! I'm Gizmo. \n"
+            + "    How may I assist you today?\n"
+            + "    ________________________________________________________________________________________\n";
         System.out.println(banner);
+        runEcho();
+    }
+
+    private static void runEcho(){
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine();
+
+        while (!command.strip().equals("bye")){
+            System.out.println(
+                "    ____________________________________________________________________________________\n"
+                + "    " + command + "\n"
+                + "    ____________________________________________________________________________________\n"
+            );
+            command = in.nextLine();
+        }
+
+        System.out.println(
+            "    ________________________________________________________________________________________\n"
+            + "    Disconnecting... I'll be here whenever you need me again.\n"
+            + "    ________________________________________________________________________________________\n"
+        );
     }
 }
+
