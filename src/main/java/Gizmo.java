7025a10ae3b257cbd88b4d0ec57866602fbee1bb
Rd-7r <mjiahao7r@gmail.com>
2026-08-29 22:04:59 +0800

    implemented mark command

--- src/main/java/Gizmo.java
+++ src/main/java/Gizmo.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Gizmo {
     private static ArrayList<String> taskList = new ArrayList<>();
+    private static ArrayList<Boolean> taskCheckList = new ArrayList<>();
 
     public static void main() {
         String banner = "    ________________________________________________________________________________________\n"
@@ -10,18 +11,29 @@ public class Gizmo {
             + "    How may I assist you today?\n"
             + "    ________________________________________________________________________________________\n";
         System.out.println(banner);
-        addToList(taskList);
+        handleCommands(taskList);
     }
 
-    private static void addToList(ArrayList<String> taskList){
+    private static void handleCommands(ArrayList<String> taskList){
         Scanner in = new Scanner(System.in);
-        String command = in.nextLine();
 
-        while (!command.strip().equals("bye")){
-            if (command.strip().equals("list")){
+        while (in.hasNextLine()){
+            String command = in.nextLine();
+            String trimmedCommand = command.strip();
+
+            if (trimmedCommand.equals("bye")) {
+                break;
+            }
+
+            String[] splitCommand = trimmedCommand.split("\\s+");
+
+            if (trimmedCommand.equals("list")){
                 listAllTasks(taskList);
+            }else if (splitCommand.length == 2 && splitCommand[0].equals("mark") && isInteger(splitCommand[1])){
+                handleMarkCommands(taskCheckList, Integer.parseInt(splitCommand[1]) - 1);
             }else {
                 taskList.add(command);
+                taskCheckList.add(false);
                 System.out.println(
                     "    ____________________________________________________________________________________\n"
                     + "    added: " + command + "\n"
@@ -29,7 +41,6 @@ public class Gizmo {
                     + "    ____________________________________________________________________________________\n"
                 );
             }
-            command = in.nextLine();
         }
 
         System.out.println(
@@ -40,21 +51,44 @@ public class Gizmo {
     }
 
     private static void listAllTasks(ArrayList<String> taskList){
-        int taskNumber = 0;
         System.out.println("    ________________________________________________________________________________________");
 
         if (taskList.isEmpty()){
-            System.out.println("wow, Such empty!");
+            System.out.println("    wow, Such empty!");
         }else {
-            for (String task : taskList) {
-                taskNumber++;
-
+            for (int i=0; i<taskList.size(); i++) {
+                String status = taskCheckList.get(i) ? "[X]" : "[ ]";
                 System.out.println(
-                        "    " + taskNumber + ". " + task
+                        "    " + (i+1) + ". " + status + " " + taskList.get(i)
                 );
             }
         }
         System.out.println("    ________________________________________________________________________________________\n");
+    }
+
+    private static void handleMarkCommands(ArrayList<Boolean> taskCheckList, int taskListIndex){
+        System.out.println("    ________________________________________________________________________________________");
+        if (taskListIndex >= taskCheckList.size() || taskListIndex < 0){
+            System.out.println("    failed: You tried to mark a non-existent task");
+        }else if (taskCheckList.get(taskListIndex)){
+            System.out.println("    you tried to mark a marked item");
+        }else{
+            System.out.println(
+                "    Nice, I'll be marking this as done!:\n"
+                + "    [X] " + taskList.get(taskListIndex)
+            );
+            taskCheckList.set(taskListIndex, true);
+        }
+        System.out.println("    ________________________________________________________________________________________\n");
+    }
+
+    private static boolean isInteger(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
 
